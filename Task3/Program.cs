aa1cd3f2597618acae8174477edb6ce4f23df78c
AstonDV <astondv.kms@gmail.com>
2023-01-25 23:59:41 +1000

    End task 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+// d = √ (х А – х В) 2 + (у А – у В) 2
+
+Console.Clear();
+
+Console.WriteLine("Введите координаты первой точки");
+Console.Write("Введите координату x: ");
+int x1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату y: ");
+int y1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("Введите координату x: ");
+int x2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату y: ");
+int y2 = int.Parse(Console.ReadLine()!);
+
+double result = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+result = Math.Round(result, 2);
+Console.WriteLine($"Расстояние между точками равно {result}");

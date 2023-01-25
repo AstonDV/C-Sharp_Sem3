caf3f0a4db702947ad0d977021add4537a4f5332
AstonDV <astondv.kms@gmail.com>
2023-01-26 00:36:27 +1000

    End task 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+
+Console.Clear();
+
+Console.Write("Введите любое число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+while (count <= number)
+{
+    double result = Math.Pow(count, 2);
+    Console.Write(result);
+    if (count != number)
+    Console.Write(", ");
+    count++;
+}

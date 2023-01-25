6d1eb65b5eeb0b53778a16a3fdec40c4c5c6b0e9
AstonDV <astondv.kms@gmail.com>
2023-01-25 23:10:25 +1000

    End task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,31 +7,21 @@ Console.Write("Введите номер четверти: ");
 int quarter = int.Parse(Console.ReadLine()!);
 
 if (quarter < 1 || quarter > 4)
-{
-    Console.WriteLine("Введенное число не является четвертью, нужно ввести число от 1 до 4, попробуйте снова");
-}
+Console.WriteLine("Введенное число не является четвертью, нужно ввести число от 1 до 4, попробуйте снова");
 else
 {
     if (quarter < 3)
     {
         if (quarter == 1)
-        {
-            Console.WriteLine("Диапозон координат в первой четверти: x > 0, y > 0");
-       }
+        Console.WriteLine("Диапозон координат в первой четверти: x > 0, y > 0");
         else
-        {
-            Console.WriteLine("Диапозон координат во второй четверти: x < 0, y > 0");
-        }
+        Console.WriteLine("Диапозон координат во второй четверти: x < 0, y > 0");
     }
     else
     {
         if (quarter == 3)
-        {
-            Console.WriteLine("Диапозон координат в третьей четверти: x < 0, y < 0");
-        }
+        Console.WriteLine("Диапозон координат в третьей четверти: x < 0, y < 0");
         else
-        {
-            Console.WriteLine("Диапозон координат в четвертой четверти: x > 0, y < 0");
-        }
+        Console.WriteLine("Диапозон координат в четвертой четверти: x > 0, y < 0");
     }
 }

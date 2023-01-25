81370f3a364ab977ce4c7a7dea70050de6fde37c
AstonDV <astondv.kms@gmail.com>
2023-01-25 21:45:26 +1000

    End task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.Clear();
+
+Console.Write("Введите X: ");
+int x = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Y: ");
+int y = int.Parse(Console.ReadLine()!);
+
+if (y > 0)
+{
+    if (x > 0)
+    {
+        Console.WriteLine("Первая четверть");
+    }
+    else
+    {
+        Console.WriteLine("Первая четверть");
+    }
+}
+else
+{
+    if (x > 0)
+    {
+        Console.WriteLine("Четвертая четверть");
+    }
+    else
+    {
+        Console.WriteLine("Третья четверть");
+    }
+}
